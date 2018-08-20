3cf861363a0f8afdbdbd4ebba21bf95df87dd969
Mike Todisco <mtodisco@splashthat.com>
2018-08-20 17:00:50 -0400

    big plays eda

--- exploring_nflscrapR.R
+++ exploring_nflscrapR.R
@@ -6,9 +6,25 @@ pbp_2010 <- season_play_by_play(2010)
 pbp_2011 <- season_play_by_play(2011)
 pbp_2012 <- season_play_by_play(2012)
 pbp_2013 <- season_play_by_play(2013)
+pbp_2014 <- season_play_by_play(2014)
+pbp_2015 <- season_play_by_play(2015)
 pbp_2016 <- season_play_by_play(2016)
+pbp_2017 <- season_play_by_play(2017)
 
-head(pbp_2009)
+full_pbp <- rbind(pbp_2009, pbp_2010, pbp_2011, pbp_2012, 
+                  pbp_2013, pbp_2014, pbp_2015, pbp_2016, pbp_2017)
+
+full_pbp <- read.csv('full_pbp.csv', stringsAsFactors = FALSE)
+season_wins <- read.csv('season_wins.csv', stringsAsFactors = FALSE)
+
+
+sort(unique(full_pbp$posteam))
+
+fix_old_team_name <- function(x){
+  gsub("JAC", "JAX", x)
+}
+
+data.frame(lapply(full_pbp$posteam, fix_old_team_name))
 
 pbp_2009 %>%
   filter(posteam != "") %>%
@@ -41,3 +57,25 @@ sort(unique(pbp_2009$HomeTeam))
 WL_data = read_csv('season_wins.csv')
 
 head(WL_data)
+
+####### Big plays
+full_pbp$big_plays <- ifelse(full_pbp$Yards.Gained > 30, 1, 0)
+full_pbp$year <- substring(full_pbp$Date, 1, 4)
+
+big_plays_df <- full_pbp %>%
+  filter(posteam != "") %>%
+  group_by(posteam, year) %>%
+  summarise(big_plays = sum(big_plays))
+
+big_play_df_merge <- merge(big_plays_df, season_wins, by.x = c("posteam", "year"), by.y=c("Tm","yr"))
+
+with(big_play_df_merge, cor(big_plays, W))
+
+plot(with(big_play_df_merge, big_plays, W))
+
+
+with(subset(big_play_df_merge, next_season_wins != 'NA'), cor(big_plays, next_season_wins))
+
+library(corrplot)
+M <- cor(big_play_df_merge[,c('big_plays','W','SoS','SRS','OSRS','DSRS','PRE_OU')])
+corrplot(M)
